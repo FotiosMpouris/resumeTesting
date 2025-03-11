647f640bd250c9e88dbe71075fdd64dcb5fea88f
FotiosMpouris <126421111+FotiosMpouris@users.noreply.github.com>
2025-03-11 08:19:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -50,14 +50,12 @@ document.addEventListener("DOMContentLoaded", function() {
       const targetId = this.getAttribute('href').split('#')[1];
       const targetElement = document.getElementById(targetId);
       if (targetElement) {
-        const headerHeight = document.querySelector('header').offsetHeight + document.querySelector('.top-banner').offsetHeight + 20; // Offset for header and banner
+        const headerHeight = document.querySelector('header').offsetHeight + 50; // Adjust for header and banner
         const topPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - headerHeight;
         window.scrollTo({
           top: topPosition,
           behavior: 'smooth'
         });
-      } else if (this.getAttribute('href').includes('pythos.html')) {
-        window.location.href = this.getAttribute('href');
       }
     });
   });
